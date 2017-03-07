b854a7a719cd303d83c9faf0014972e1dfbdf54b
lthoang@wpi.edu <anhmiuhv@yahoo.com.vn>
2017-03-07 17:19:08 -0500

    Refractor, add matchinfoviewcontroller

---
+++ opendota/controller/MatchInfoViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MatchInfoViewController.swift
+//  opendota
+//
+//  Created by Linh Hoang on 3/7/17.
+//  Copyright © 2017 Linh Hoang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MatchInfoViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+}
